8595a417e50a793398e83395f2238fa6147eb568
davine94 <davine94>
2022-05-15 22:12:21 +0300

    Created classes of the two forms

--- app/routes.py
+++ app/routes.py
@@ -1,7 +1,20 @@
 from flask import render_template
 from app import app
+from app.forms import RegistrationForm, LoginForm
+
+app.config['SECRET_KEY'] = 'd41015350b40aadac73a9253ae9659bd'
 
 @app.route("/")
 def home():
     title = 'Daily Dose of Inspiration'
     return render_template("index.html", title=title)
+
+@app.route("/register")
+def register():
+    form = RegistrationForm()
+    return render_template('register.html', title='Register', form=form)
+
+@app.route("/login")
+def login():
+    form = LoginForm()
+    return render_template('login.html', title='Login', form=form)
