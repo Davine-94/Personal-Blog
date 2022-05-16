cd3dd920d0953147f4ec759ebb207227d3805a60
davine94 <davine94>
2022-05-16 11:37:06 +0300

    Made changes on the register and base template

--- app/routes.py
+++ app/routes.py
@@ -1,17 +1,35 @@
-from flask import render_template
+from flask import render_template, flash, redirect, url_for
 from app import app
 from app.forms import RegistrationForm, LoginForm
 
 app.config['SECRET_KEY'] = 'd41015350b40aadac73a9253ae9659bd'
 
+posts = [
+    {
+        'author': 'Corey Schafer',
+        'title': 'Blog Post 1',
+        'content': 'First post content',
+        'date_posted': 'April 20, 2018'
+    },
+    {
+        'author': 'Jane Doe',
+        'title': 'Blog Post 2',
+        'content': 'Second post content',
+        'date_posted': 'April 21, 2018'
+    }
+]
+
 @app.route("/")
 def home():
     title = 'Daily Dose of Inspiration'
-    return render_template("index.html", title=title)
+    return render_template("index.html", title=title, posts=posts)
 
-@app.route("/register")
+@app.route("/register", methods=['GET', 'POST'])
 def register():
     form = RegistrationForm()
+    if form.validate_on_submit():
+        flash(f'Account created for {form.username.data}!', 'success')
+        return redirect(url_for('home'))
     return render_template('register.html', title='Register', form=form)
 
 @app.route("/login")
